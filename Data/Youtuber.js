4927946be80de44d1f16788c6198d3349dbd4f51
Asatakaku <asagawatakaku2212@gmail.com>
2024-04-30 12:33:01 +0700

    Fix YoutubeData

--- Data/Youtuber.js
+++ Data/Youtuber.js
@@ -10,21 +10,21 @@ const Youtuber = [
         id: 2,
         username: 'AniViet', password: 'Ani',
         name: 'Ani-One Vietnam', subcriber: 116000,
-        icon: 'https://yt3.ggpht.com/hruIdUPUpkSzw2KQoe3y7jdlw8fvWIeFUAE5NlmsLsdX-OOZ-elK9qicAetZkbEcMwK0lX1Q=s48-c-k-c0x00ffffff-no-rj',
+        icon: 'https://yt3.googleusercontent.com/hruIdUPUpkSzw2KQoe3y7jdlw8fvWIeFUAE5NlmsLsdX-OOZ-elK9qicAetZkbEcMwK0lX1Q=s48-c-k-c0x00ffffff-no-rj',
         coverimage:  'https://yt3.googleusercontent.com/XojJYHc5k-77R8KBPDesFDzETqhFYRCEuWl4rLxo4E3yDNx3_28Xj_o7qrnvkyO3xk_4JV84Jvk=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj',
     },
     {
         id: 3,
         username: 'Taka', password: 'Takaku',
         name: 'Asagawa Takaku', subcriber: 4650,
-        icon: 'https://yt3.ggpht.com/dBqUHM4WQ2oTt5tkGxiwRNbu3iyZS-anVSPcgkDxadjrvJbuA5lQPhhX-1LPn9rWsTv8QPbSDA=s48-c-k-c0x00ffffff-no-rj',
+        icon: 'https://yt3.googleusercontent.com/dBqUHM4WQ2oTt5tkGxiwRNbu3iyZS-anVSPcgkDxadjrvJbuA5lQPhhX-1LPn9rWsTv8QPbSDA=s48-c-k-c0x00ffffff-no-rj',
         coverimage: 'https://yt3.googleusercontent.com/8YcGmqFHu86mzNfDlZDOHbpDPVsNPIf0bbwjyk9iW_ZWAvY07DdDbCLB-AodkbeMynhDxsKL-s8=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj'
     },
     {
         id: 4,
         username: 'Stiv', password: 'Stiv',
         name: 'Stiv Anim 2.0', subcriber: 10300,
-        icon: 'https://yt3.ggpht.com/LhRdnV4D4iUApFox6Y6LvHf-vE3U6z-heb3KLMV70LBqZR-PGoD8df8dp_RjNuFWLsLs8zf1O_U=s48-c-k-c0x00ffffff-no-rj',
+        icon: 'https://yt3.googleusercontent.com/LhRdnV4D4iUApFox6Y6LvHf-vE3U6z-heb3KLMV70LBqZR-PGoD8df8dp_RjNuFWLsLs8zf1O_U=s48-c-k-c0x00ffffff-no-rj',
         coverimage: 'https://yt3.googleusercontent.com/XOkUYkziDAx5pSNwbZEA68n8YzZzfkADEwIKIRJGkYyxBZWir-mvctadeZLT3ZfOLCzCXi108qY=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj'
     }
 ]
